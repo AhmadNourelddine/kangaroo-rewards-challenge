0281ed6432367e9671965f2c951944b514fb1c80
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-05-01 16:25:09 +0300

    all surveys page

---
+++ kangaroo-rewards-challenge-frontend/src/pages/AllSurveys/index.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
+
+const AllSurveys = () => {
+  const [surveys, setSurveys] = useState([]);
+
+  const navigate = useNavigate();
+  const getSurveys = async () => {
+    await axios
+      .get("http://127.0.0.1:8000/api/survey-types")
+      .then((response) => {
+        setSurveys(response.data);
+        console.log(response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getSurveys();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Surveys</h1>
+      {surveys && (
+        <table style={{ margin: "auto" }}>
+          <tr>
+            <th>Survey Name</th>
+            <th>Survey Code</th>
+          </tr>
+          {surveys.map((item) => (
+            <tr>
+              <td>
+                <button
+                  onClick={() => {
+                    window.localStorage.setItem("name", item["name"]);
+                    window.localStorage.setItem("code", item["code"]);
+                    navigate("/survey");
+                  }}
+                >
+                  {item["name"]}
+                </button>
+              </td>
+              <td>{item["code"]}</td>
+            </tr>
+          ))}
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default AllSurveys;
