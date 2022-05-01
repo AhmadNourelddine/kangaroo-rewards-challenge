2fbbb5e5235df4cec341f6395ba60fe24d695c68
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-05-01 16:24:53 +0300

    added router and routes

--- kangaroo-rewards-challenge-frontend/src/App.js
+++ kangaroo-rewards-challenge-frontend/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Routes, Route } from "react-router-dom";
 import * as Survey from "survey-react";
 import "survey-react/survey.css";
 import SurveyComponent from "./pages/survey";
+import AllSurveys from "./pages/AllSurveys";
 
 function App() {
   return (
     <div>
-      <SurveyComponent />
+      <Routes>
+        <Route exact path="/*" element={<AllSurveys />} />
+        <Route exact path="/survey" element={<SurveyComponent />} />
+      </Routes>
     </div>
   );
 }
