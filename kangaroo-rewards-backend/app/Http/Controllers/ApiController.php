fbe262460b320c4005cd954d5fb5f4a85a03ac8d
AhmadNourelddine <ahmad.nourelddine@gmail.com>
2022-05-01 16:25:46 +0300

    check survey answers

--- kangaroo-rewards-backend/app/Http/Controllers/ApiController.php
+++ kangaroo-rewards-backend/app/Http/Controllers/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class ApiController extends Controller
 {
@@ -18,19 +19,19 @@ class ApiController extends Controller
     }
 
     function surveyQuestions(Request $request){
+
         $code = $request->code;
         $file_name = substr($code, -1); 
         $file_path = storage_path()."/data_files/".$file_name.".json";
         $survey = json_decode(file_get_contents($file_path), true);
-        for($i=0; $i<sizeof($answers["questions"]); $i++){
+        for($i=0; $i<sizeof($survey["questions"]); $i++){
             unset($survey["questions"][$i]["answer"]);
             unset($survey["questions"][$i]["answer"]);
         }
         return response()->json($survey["questions"]);
     }
 
-    function surveyAnswers(Request $request){
-        $answers = $request->answers;
+    function surveyAnswers(Request $answers){
 
         $code = $answers["survey"]["code"];
         $file_name = substr($code, -1); 
@@ -38,13 +39,19 @@ class ApiController extends Controller
         $survey = json_decode(file_get_contents($file_path), true);
 
         $result=[];
+
         for($i=0; $i<sizeof($answers["questions"]); $i++){
             $user_answers = $answers["questions"][$i]["answer"];
-            $correct_answers = $answers["questions"][$i]["answer"];
+            $correct_answers = $survey["questions"][$i]["answer"];
             if($user_answers === $correct_answers){
                 $array= array(
                             $answers["questions"][$i]["type"] => true,
                 );
+                $result = array_merge($result, $array);
+            }else{
+                $array= array(
+                    $answers["questions"][$i]["type"] => false,
+                 );
                 $result = array_merge($result, $array);
             }
         }
